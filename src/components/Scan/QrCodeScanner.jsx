32eed9d43d7b9a73516f238e59dbe28ee7181abb
didsqq <adelgaripov6@gmail.com>
2025-03-18 12:42:28 +0300

    1

--- src/components/Scan/QrCodeScanner.jsx
+++ src/components/Scan/QrCodeScanner.jsx
@@ -2,18 +2,25 @@ import { Scanner } from '@yudiel/react-qr-scanner';
 import { useState } from 'react';
 import s from './qrCodeScanner.module.css';
 
+import { SCAN_DATA } from '../../constants'
+
 export const QrCodeScanner = () => {
     const [result, setResult] = useState('')
 
     const scanHandler = (result) => {
-        console.log(result)
         setResult(result[0].rawValue);
+
+        const prevData = JSON.parse(localStorage.getItem(SCAN_DATA) || '[]');
+
+        localStorage.setItem(
+            SCAN_DATA, 
+            JSON.stringify([...prevData, result[0].rawValue])
+        );
     };
 
     return (
         <div className={s.container}>
             <Scanner 
-                allowMultiple 
                 onScan={scanHandler}
                 components={{
                     finder: false,
